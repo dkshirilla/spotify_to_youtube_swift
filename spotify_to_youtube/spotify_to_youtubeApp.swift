435ffa6c174dded0aa44bb3a96455073ce7dd2db
Doug <douglasshirilla@proton.me>
2023-11-26 23:42:27 -0500

    Initial Commit

---
+++ spotify_to_youtube/spotify_to_youtubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_to_youtubeApp.swift
+//  spotify_to_youtube
+//
+//  Created by Douglas Shirilla on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct spotify_to_youtubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
