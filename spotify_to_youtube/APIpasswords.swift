d8cd744a73da8318d2e528bdbf70a3f2090d8803
Doug <douglasshirilla@proton.me>
2023-11-30 23:11:40 -0500

    more work

---
+++ spotify_to_youtube/APIpasswords.swift
@@ -0,0 +1,8 @@
+//
+//  APIpasswords.swift
+//  spotify_to_youtube
+//
+//  Created by Douglas Shirilla on 11/30/23.
+//
+
+
--- spotify_to_youtube/ContentView.swift
+++ spotify_to_youtube/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import OAuth2
+import OAuthSwift
 
 struct ContentView: View {
     var body: some View {
@@ -13,9 +15,45 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("doug!")
+            Text("Boobs!")
         }
         .padding()
+        
+        Button(action: {
+            
+
+            // create an instance and retain it
+            let oauthswift = OAuth2Swift(
+                consumerKey:    SpotifyClientId,
+                consumerSecret: SpotifyClientSecret,
+                authorizeUrl: "https://accounts.spotify.com/authorize",
+                responseType: "code"
+            )
+            oauthswift.accessTokenBasicAuthentification = true
+
+            guard let codeVerifier = generateCodeVerifier() else {return}
+            guard let codeChallenge = generateCodeChallenge(codeVerifier: codeVerifier) else {return}
+
+            let handle = oauthswift.authorize(
+                withCallbackURL: "spotify-to-youtube://spotify-to-youtube/",
+                scope: "playlist-read-private",
+                state:"State01",
+                codeChallenge: codeChallenge,
+                codeChallengeMethod: "S256",
+                codeVerifier: codeVerifier) { result in
+                switch result {
+                case .success(let (credential, response, parameters)):
+                  print(credential.oauthToken)
+                  // Do your request
+                case .failure(let error):
+                  print(error.localizedDescription)
+                }
+            }
+            
+                    
+        }, label: {
+            Text("boobs #2")
+        })
     }
 }
 
